6df8b8e5b3729ba2e063e99e4f2598e3855901a2
Jack Richard <jackrichardbusiness@gmail.com>
2022-01-19 11:54:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const skills = [
+  ["A web designer.", "#69FFF1"],
+  ["A Java programmer.", "#63D471"],
+  ["A game developer.", "#E4C1F9"],
+  ["A 3D artist.", "#F7A072"],
+  ["An IT specialist.", "#9E2A2B"],
+  ["A laptop repairer.", "#E4C1F9"]
+];
+var skillIx = 0;
+
+function typeOut(i) {
+  document.querySelector("#skill").innerHTML = skills[skillIx][0].slice(
+    0,
+    i + 1
+  );
+}
+
+setTimeout(nextSkill, 3500);
+function nextSkill() {
+  if (skillIx + 1 > skills.length - 1) {
+    skillIx = 0;
+  } else {
+    skillIx++;
+  }
+
+  var skillHTML = document.querySelector("#skill");
+  var initialLength = skillHTML.innerHTML.length;
+  for (var i = 0; i < document.querySelector("#skill").innerHTML.length; i++) {
+    setTimeout(function () {
+      document.querySelector("#skill").innerHTML = skillHTML.innerHTML.slice(
+        0,
+        skillHTML.innerHTML.length - 1
+      );
+    }, 50 * i);
+  }
+
+  setTimeout(function () {
+    document.querySelector("#skill").style.color = skills[skillIx][1];
+
+    for (var i = 0; i < skills[skillIx][0].length; i++) {
+      setTimeout(typeOut.bind(null, i), 100 + i * 50);
+    }
+  }, initialLength * 50 + 100);
+  setTimeout(nextSkill, (initialLength * 50) + (skills[skillIx][0].length * 50) + 2500);
+}
